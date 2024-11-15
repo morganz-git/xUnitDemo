bd9a737871b2e2078f1481e9453f181b83988db8
Morgan Zhang <99776999+morganggl@users.noreply.github.com>
2024-11-15 23:37:01 +1300

    使用 AutoFixture 库生成一个 RegisterUserModel 对象。AutoFixture 可以自动生成测试数据，简化测试用例的编写。

--- xUnitDemo/RegisterUserModel.cs
+++ xUnitDemo/RegisterUserModel.cs
@@ -1,6 +1,11 @@
-﻿namespace xUnitDemo;
+﻿using System.Security.AccessControl;
+
+namespace xUnitDemo;
 
 public class RegisterUserModel
 {
-    
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public string CPassword { get; set; }
+    public string  Email { get; set; }
 }
--- xUnitDemo/SeleniumWithAutoFixtureData.cs
+++ xUnitDemo/SeleniumWithAutoFixtureData.cs
@@ -21,17 +21,34 @@ public class SeleniumWithAutoFixtureData : IClassFixture<WebDriverFixture>
     {
         var driver = _webDriverFixture.ChromeDriver;
         driver.Navigate().GoToUrl("http://eaapp.somee.com");
-        
-        var userName=new Fixture().Create<string>();
+
+        var userName = new Fixture().Create<string>();
         var password = new Fixture().Create<string>();
         var email = new Fixture().Create<string>();
-        
-        
+
+
         driver.FindElement(By.Id("registerLink")).Click();
         driver.FindElement(By.Id("UserName")).SendKeys(userName);
         driver.FindElement(By.Id("Password")).SendKeys(password);
         driver.FindElement(By.Id("ConfirmPassword")).SendKeys(password);
         driver.FindElement(By.Id("Email")).SendKeys(email);
+        _testOutputHelper.WriteLine("Test Done");
+    }
+
+    [Fact]
+    public void TestRegisterUserWithType()
+    {
+        var driver = _webDriverFixture.ChromeDriver;
+        driver.Navigate().GoToUrl("http://eaapp.somee.com");
+
+        // 使用 AutoFixture 库生成一个 RegisterUserModel 对象。AutoFixture 可以自动生成测试数据，简化测试用例的编写。
+        var model = new Fixture().Create<RegisterUserModel>();
+
+        driver.FindElement(By.Id("registerLink")).Click();
+        driver.FindElement(By.Id("UserName")).SendKeys(model.Name);
+        driver.FindElement(By.Id("Password")).SendKeys(model.Password);
+        driver.FindElement(By.Id("ConfirmPassword")).SendKeys(model.CPassword);
+        driver.FindElement(By.Id("Email")).SendKeys(model.Email);
         _testOutputHelper.WriteLine("Test Done");
     }
 }
